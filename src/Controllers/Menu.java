f36229db695e384049f34635c297497ee4f0c9d8
NatalyMora21 <tatamora21gmail.com>
2021-09-09 14:28:34 -0500

    Creada función que pide por parametros por consola y luego llama a la función que valida el número mayor

--- src/Controllers/Menu.java
+++ src/Controllers/Menu.java
@@ -1,2 +1,13 @@
-package Controllers;public class Menu {
+package Controllers;
+import java.util.Scanner;
+
+public class Menu {
+
+    public static void numeroMayor(Scanner sc) {
+        System.out.println("Ingrese el primer número");
+        double num1 = sc.nextDouble();
+        System.out.println("Ingrese el segundo número");
+        double num2 = sc.nextDouble();
+        Ejercicios.numeroMayorp(num1, num2);
+    }
 }
