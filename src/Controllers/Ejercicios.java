fc2a97e44be06a7f9d00c456c4258ef2f9c7cf8d
NatalyMora21 <tatamora21gmail.com>
2021-09-09 14:29:31 -0500

    Creación de funciones que calculan el número mayor

--- src/Controllers/Ejercicios.java
+++ src/Controllers/Ejercicios.java
@@ -2,7 +2,21 @@ package Controllers;
 
 public class Ejercicios {
 
-    public static void numeroMayor(double num1 , double num2){
+    public static void numeroMayor(){
+
+        double num1 = 5;
+        double num2 = 6;
+        if (num1 > num2){
+            System.out.println("El número mayor es : " + num1);
+        }
+        else if (num1 < num2 ){
+            System.out.println("El número mayor es : " + num2);
+        }
+        else {
+            System.out.println("Los números son iguales");
+        }
+    }
+    public static void numeroMayorp(double num1 , double num2){
         if (num1 > num2){
             System.out.println("El número mayor es : " + num1);
         }
