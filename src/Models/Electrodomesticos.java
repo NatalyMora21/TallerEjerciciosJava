913b92f9147d857a4b65d29df3dc73d7c6f592f2
NatalyMora21 <tatamora21gmail.com>
2021-09-09 22:12:25 -0500

    Creación de clase Electrodomestico con sus diferentes métodos

--- src/Models/Electrodomesticos.java
+++ src/Models/Electrodomesticos.java
@@ -1,2 +1,133 @@
-package Models;public class Electrodomesticos {
+package Models;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Electrodomesticos {
+    private String precio;
+    private double base;
+    private String color;
+    private char consumo;
+    private double peso;
+    private double tamano;
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public char getConsumo() {
+        return consumo;
+    }
+
+    public void setConsumo(char consumo) {
+        this.consumo = consumo;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+
+    public Electrodomesticos(String precio, double base, String color, char consumo, double peso) {
+        this.precio = precio;
+        this.peso = peso;
+        this.color = color;
+        this.base = base;
+        this.consumo = consumo;
+    }
+
+    public Electrodomesticos(String precio, double peso) {
+        this.precio = precio;
+        this.peso = peso;
+        this.color = "blanco";
+        this.base = 100;
+    }
+
+    public Electrodomesticos(){
+        this.color = "blanco";
+        this.base = 100;
+        this.peso = 5;
+    }
+
+    public void comprobarConsumoEnergetico (char letra) {
+
+        if (this.consumo != 'A'){
+            if (this.consumo != 'B'){
+                this.consumo = 'F';
+            }
+        }
+
+    }
+
+    public void comprobarColor(String color){
+
+    }
+
+    public int precioFinal(){
+        int precioConsumo=0;
+        switch (this.consumo){
+            case 'A':
+                precioConsumo = 100;
+                break;
+            case 'B':
+                precioConsumo = 80;
+                break;
+            case 'C':
+                precioConsumo = 60;
+                break;
+            case 'D':
+                precioConsumo = 50;
+                break;
+            case 'E':
+                precioConsumo= 30;
+                break;
+            case 'F':
+                precioConsumo=100;
+                break;
+        }
+        return precioConsumo;
+    }
+
+    public int precioTamaño(){
+
+        int precioTamano = 0;
+
+        if (this.tamano>0 && this.tamano <=19){
+            precioTamano = 10;
+        }
+        else if (this.tamano>=20 && this.tamano <=49 ) {
+            precioTamano = 50;
+        }
+        else if (this.tamano>=50 && this.tamano <=79 ) {
+            precioTamano = 80;
+        }
+        else if (this.tamano >= 80 ) {
+            precioTamano = 100;
+        }
+        return precioTamano;
+    }
+
 }
--- src/Models/Persona.java
+++ src/Models/Persona.java
@@ -1,2 +1,119 @@
-package Models;public class Persona {
+package Models;
+
+public class Persona {
+    private String nombre;
+    private int edad;
+    private String DNI;
+    private char sexo;
+    private double peso;
+    private double altura;
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public Persona() {
+        this.nombre= "";
+        this.edad = 0;
+        this.sexo = ' ';
+        this.DNI = "";
+        this.peso= 0;
+        this.altura = 0;
+    }
+
+    public Persona(String nombre, int edad,char sexo){
+        this.nombre= nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.DNI = "";
+        this.peso= 0;
+        this.altura = 0;
+    }
+
+    public Persona(String nombre, int edad, char sexo, double peso, double altura, String DNI ) {
+        this.nombre= nombre;
+        this.edad = edad;
+        this.DNI = DNI;
+        this.sexo = sexo;
+        this.peso= peso;
+        this.altura = altura;
+    }
+
+    public int calcularMC(){
+        double calPesoIdeal = this.peso/(Math.pow(this.altura,2));
+        int pesoIdeal = 0;
+        if (calPesoIdeal < 20) {
+            pesoIdeal = -1;
+        }
+        else if(calPesoIdeal >= 20 && calPesoIdeal <=25){
+            pesoIdeal = 0;
+        }
+        else if (calPesoIdeal>25){
+            pesoIdeal = 1;
+        }
+        return pesoIdeal;
+    }
+
+    public boolean esMayorDeEdad(){
+        boolean esMayor = this.edad > 18 ? true : false;
+        return  esMayor;
+
+    }
+
+    private void comprobarSexo() {
+        if (this.sexo != 'F' || this.sexo != 'M'){
+            this.sexo= 'M';
+        }
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String toString(){
+
+        return ("H");
+    }
+
+    /*private String  generarDNI(){
+
+    }*/
+
 }
